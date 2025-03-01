43c91e6b2bf56ff4535e7a0afdb3db2851630172
JaKooLit <ejhay.games@gmail.com>
2025-03-02 01:26:43 +0900

    initial 24.04 whiptail version

--- install-scripts/sddm_theme.sh
+++ install-scripts/sddm_theme.sh
@@ -2,6 +2,19 @@
 # 💫 https://github.com/JaKooLit 💫 #
 # SDDM themes for ubuntu 24.04 #
 
+## WARNING: DO NOT EDIT BEYOND THIS LINE IF YOU DON'T KNOW WHAT YOU ARE DOING! ##
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Change the working directory to the parent directory of the script
+PARENT_DIR="$SCRIPT_DIR/.."
+cd "$PARENT_DIR" || { echo "${ERROR} Failed to change directory to $PARENT_DIR"; exit 1; }
+
+# Source the global functions script
+if ! source "$(dirname "$(readlink -f "$0")")/Global_functions.sh"; then
+  echo "Failed to source Global_functions.sh"
+  exit 1
+fi
+
 printf "\n%s - Installing ${SKY_BLUE}Simple SDDM Theme${RESET}\n" "${NOTE}"
 
 # Set the theme variable
