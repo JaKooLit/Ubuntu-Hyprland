c113ebcbfe3070286b012b8c2220d1e771449920
Don Williams <don.e.williams@gmail.com>
2025-10-19 00:39:58 -0400

    Hyprdots pull from main branch

--- install-scripts/dotfiles-branch.sh
+++ install-scripts/dotfiles-branch.sh
@@ -3,37 +3,40 @@
 # Hyprland-Dots to download from main #
 
 #specific branch or release
-dots_tag="Ubuntu-24.04-Dots"
+dots_tag="main"
 
 ## WARNING: DO NOT EDIT BEYOND THIS LINE IF YOU DON'T KNOW WHAT YOU ARE DOING! ##
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # Change the working directory to the parent directory of the script
 PARENT_DIR="$SCRIPT_DIR/.."
-cd "$PARENT_DIR" || { echo "${ERROR} Failed to change directory to $PARENT_DIR"; exit 1; }
+cd "$PARENT_DIR" || {
+    echo "${ERROR} Failed to change directory to $PARENT_DIR"
+    exit 1
+}
 
 # Source the global functions script
 if ! source "$(dirname "$(readlink -f "$0")")/Global_functions.sh"; then
-  echo "Failed to source Global_functions.sh"
-  exit 1
+    echo "Failed to source Global_functions.sh"
+    exit 1
 fi
 
 # Check if Hyprland-Dots exists
 printf "${NOTE} Downloading KooL's Hyprland Dots for Ubuntu 24.04....\n"
 
 if [ -d Hyprland-Dots-Ubuntu-24.04 ]; then
-  cd Hyprland-Dots-Ubuntu-24.04
-  git stash && git pull
-  chmod +x copy.sh
-  ./copy.sh 
-else
-  if git clone --depth=1 -b $dots_tag https://github.com/JaKooLit/Hyprland-Dots Hyprland-Dots-Ubuntu-24.04; then
-    cd Hyprland-Dots-Ubuntu-24.04 || exit 1
+    cd Hyprland-Dots-Ubuntu-24.04
+    git stash && git pull
     chmod +x copy.sh
-    ./copy.sh 
-  else
-    echo -e "$ERROR Can't download Hyprland-Dots-Ubuntu-24.04"
-  fi
+    ./copy.sh
+else
+    if git clone --depth=1 https://github.com/JaKooLit/Hyprland-Dots Hyprland-Dots-Ubuntu-24.04; then
+        cd Hyprland-Dots-Ubuntu-24.04 || exit 1
+        chmod +x copy.sh
+        ./copy.sh
+    else
+        echo -e "$ERROR Can't download Hyprland-Dots-Ubuntu-24.04"
+    fi
 fi
 
 printf "\n%.0s" {1..2}
